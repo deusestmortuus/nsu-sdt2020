c26ce62e17de5519e7c7466383c326cf5f58428c
Антон Кузнецов <59287316+deusestmortuus@users.noreply.github.com>
2020-11-28 04:14:47 +0700

    Update lab3_2.clj
    Исправил замечания

--- lab3/lab3_2.clj
+++ lab3/lab3_2.clj
@@ -5,41 +5,30 @@
     (Thread/sleep sleep)
     (apply pred coll)))
 
+(def my-heavy-pred (heavy 100 even?))
+
 (defn block-delimiter [size-block coll]
   (lazy-seq (if (> (count (take size-block coll)) 0)
-      (cons (take size-block coll) (block-delimiter size-block (drop size-block coll)))
-      ())))
-
-(defn threads-delimiter [threads coll]
-  (let [n (Math/round (double (/ (count coll) threads)))]
-    (loop [acc [] new_coll coll]
-      (if (= (inc (count acc)) threads)
-        (conj acc new_coll)
-        (recur (conj acc (take n new_coll)) (drop n new_coll))))))
+              (cons (take size-block coll) (block-delimiter size-block (drop size-block coll)))
+              ())))
 
 (defn my-filter [pred]
   (fn [coll]
-    (let [pred (heavy 100 pred)]
-      (->>
-        coll
-        (map #(future (doall (filter pred %))))
-        (doall)
-        (map deref)
-        (apply concat)
-        (lazy-seq)))))
+    (->>
+      coll
+      (map #(future (doall (filter pred %))))
+      (doall)
+      (mapcat deref)
+      (lazy-seq))))
 
-(defn my-filter-ls [threads block-size pred coll]
+(defn my-filter-ls [mini-block-size block-size pred coll]
   (->>
-    (block-delimiter block-size coll) ;((1 2 3 4 5) (6 7 8 9 10))
-    (map #(threads-delimiter threads %)) ;((1 2 3) (4 5)) - 300+ 600+
-    (map #((my-filter pred) %))
-    (apply concat)))
+    (block-delimiter block-size coll)                       ;((1 2 3 4 5) (6 7 8 9 10))
+    (map #(block-delimiter mini-block-size %))              ;((1 2 3) (4 5))
+    (mapcat #((my-filter pred) %))))
 
 (defn main []
-  (let [f (my-filter-ls 2 3 even? (range))]
-    (time (println (doall (take 4 f))))
-    ))
+  (let [f (my-filter-ls 2 3 my-heavy-pred (range))]
+    (time (println (doall (take 4 f))))))
 
 (main)
-
-
